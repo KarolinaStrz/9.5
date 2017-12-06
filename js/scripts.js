cdd3e965961d8478c0c7fba95e4c4c3ebfae327d
Karolina Strzelecka <kstrzelecka0594@gmail.com>
2017-12-06 17:54:07 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+/*var withButtonItemsClass = document.getElementsByClassName('button');
+
+console.log(withButtonItemsClass);
+
+for (var i = 0; i <= withButtonItemsClass.length; i++) {
+	console.log(withButtonItemsClass[0].InnerText);
+}
+
+*/
+var withButtonItemsClass = document.getElementsByClassName('button');
+console.log(withButtonItemsClass[0].innerHTML);
